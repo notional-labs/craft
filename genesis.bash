a5e11558967ea5c8ba0e3e6d03fde186fdfb7c4f
Jacob Gadikian <jacobgadikian@gmail.com>
2022-02-05 04:07:32 +0700

    Update genesis.bash

--- genesis.bash
+++ genesis.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for word in $(cat genesis.txt); do craftd add-genesis-account $(echo $word) --vesting-amount 10000exp --permanent; done
+for word in $(cat genesis.txt); do craftd add-genesis-account $(echo $word) --vesting-amount 10000exp --account-type perm-locked; done
