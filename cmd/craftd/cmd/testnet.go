980d808f4849b9d20cc653e7255164899d41dce0
Reece <reecepbcups@gmail.com>
2022-02-19 16:48:34 -0600

    exp-prop.json update. Cant find how to make type

--- cmd/craftd/cmd/testnet.go
+++ cmd/craftd/cmd/testnet.go
@@ -530,12 +530,9 @@ func startTestnet(cmd *cobra.Command, args startArgs) error {
 		return err
 	}
 
-	height, err := testnet.WaitForHeight(200)
-	if err != nil {
-		return err
-	}
-
+	height, _ := testnet.WaitForHeight(200)
 	fmt.Printf("Testnet started (height=%d).\n", height)
+
 	cmd.Println("press the Enter Key to terminate")
 	fmt.Scanln() // wait for Enter Key
 	testnet.Cleanup()
