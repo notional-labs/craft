60a05afd00e9717b97c03820879c665969e702b5
Reece <reecepbcups@gmail.com>
2022-02-27 22:39:47 -0600

    Future note to add craft for genesis DAO members

--- genesis.bash
+++ genesis.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# Need to also add 250k craft for each DAO member after we get EXP worked out
 for word in $(cat genesis.txt); do craftd add-genesis-account $(echo $word) 10000exp --account-type perm-locked; done
