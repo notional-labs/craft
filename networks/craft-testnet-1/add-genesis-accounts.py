f62e910dbf9150a8c294b9150e135aedb9406991
Reece <reecepbcups@gmail.com>
2022-03-28 16:00:46 -0500

    Better genesis automation

--- networks/craft-testnet-1/add-genesis-accounts.py
+++ networks/craft-testnet-1/add-genesis-accounts.py
@@ -1,8 +1,23 @@
+from operator import ge
 import os
 import json
-from pydoc import cram
+from pathlib import Path
 
 # cd networks/craft-testnet-1/
+
+GENESIS_FILE=f"{Path.home()}/.craftd/config/genesis.json"
+
+# load genesis.json & remove all values for accounts & supply
+with open(GENESIS_FILE) as f:
+    genesis = json.load(f)
+    genesis["app_state"]['auth']["accounts"] = []
+    genesis["app_state"]['bank']["balances"] = []
+    genesis["app_state"]['bank']["supply"] = []
+
+# save genesis.json
+with open(GENESIS_FILE, 'w') as f:
+    json.dump(genesis, f, indent=4)
+
 
 gentx_files = os.listdir('gentx')
 for file in gentx_files:
