87ef404b83257edf2fdb6de12af96b18e05dd899
Reece <reecepbcups@gmail.com>
2022-03-28 16:30:30 -0500

    genesis script - quicker usability

--- networks/craft-testnet-1/add-genesis-accounts.py
+++ networks/craft-testnet-1/add-genesis-accounts.py
@@ -5,31 +5,59 @@ from pathlib import Path
 
 # cd networks/craft-testnet-1/
 
+LAUNCH_TIME = "2022-03-29T21:23:00Z"
+CHAIN_ID = "craft-testnet-1"
 GENESIS_FILE=f"{Path.home()}/.craftd/config/genesis.json"
 
-# load genesis.json & remove all values for accounts & supply
-with open(GENESIS_FILE) as f:
-    genesis = json.load(f)
-    genesis["app_state"]['auth']["accounts"] = []
-    genesis["app_state"]['bank']["balances"] = []
-    genesis["app_state"]['bank']["supply"] = []
+def main():
+    outputDetails()
+    resetGenesisFile()
+    createGenesisAccountsCommands()
 
-# save genesis.json
-with open(GENESIS_FILE, 'w') as f:
-    json.dump(genesis, f, indent=4)
+def resetGenesisFile():
+    # load genesis.json & remove all values for accounts & supply
+    with open(GENESIS_FILE) as f:
+        genesis = json.load(f)
+        genesis["genesis_time"] = LAUNCH_TIME
+        genesis["chain_id"] = str(CHAIN_ID)
+
+        genesis["app_state"]['auth']["accounts"] = []
+        genesis["app_state"]['bank']["balances"] = []
+        genesis["app_state"]['bank']["supply"] = []
+
+    # save genesis.json
+    with open(GENESIS_FILE, 'w') as f:
+        json.dump(genesis, f, indent=4)
 
 
-gentx_files = os.listdir('gentx')
-for file in gentx_files:
-    f = open('gentx/' + file, 'r')
-    data = json.load(f)
+def outputDetails() -> str:
+    # get the seconds until LAUNCH_TIME
+    launch_time = int(os.popen("date -d '" + LAUNCH_TIME + "' +%s").read())
+    now = int(os.popen("date +%s").read())
+    seconds_until_launch = launch_time - now
 
-    validatorData = data['body']['messages'][0]
-    moniker = validatorData['description']['moniker']
-    delegator = validatorData['delegator_address']
-    amt = validatorData['value']['amount']
+    # convert seconds_until_launch to hours, minutes, and seconds
+    hours = seconds_until_launch // 3600
+    minutes = (seconds_until_launch % 3600) // 60
 
-    if delegator == "craft13vhr3gkme8hqvfyxd4zkmf5gaus840j5hwuqkh":
-        print(f"craftd add-genesis-account {delegator} 100000000000000ucraft,30000000000uexp #pbcups")
-    else:
-        print(f"craftd add-genesis-account {delegator} {amt}uexp #{moniker}")
+    print(f"# {LAUNCH_TIME=} ({hours}h {minutes}m) from now\n# {CHAIN_ID=}\n")
+
+def createGenesisAccountsCommands():
+    gentx_files = os.listdir('gentx')
+    for file in gentx_files:
+        f = open('gentx/' + file, 'r')
+        data = json.load(f)
+
+        validatorData = data['body']['messages'][0]
+        moniker = validatorData['description']['moniker']
+        delegator = validatorData['delegator_address']
+        amt = validatorData['value']['amount']
+
+        if delegator == "craft13vhr3gkme8hqvfyxd4zkmf5gaus840j5hwuqkh":
+            print(f"craftd add-genesis-account {delegator} 100000000000000ucraft,30000000000uexp #pbcups")
+        else:
+            print(f"craftd add-genesis-account {delegator} {amt}uexp #{moniker}")
+
+
+if __name__ == "__main__":
+    main()
