5d7443718548497901adfd7a48f96962ddc01385
Reece <reecepbcups@gmail.com>
2022-04-19 21:56:43 -0500

    Server paying player on chain

--- minecraft-integration/craft-integration/src/main/java/com/crafteconomy/blockchain/core/request/BlockchainRequest.java
+++ minecraft-integration/craft-integration/src/main/java/com/crafteconomy/blockchain/core/request/BlockchainRequest.java
@@ -11,6 +11,7 @@ import com.crafteconomy.blockchain.transactions.PendingTransactions;
 import com.crafteconomy.blockchain.transactions.Tx;
 import com.crafteconomy.blockchain.utils.Util;
 
+import org.apache.commons.lang.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,8 +25,9 @@ public class BlockchainRequest {
     private static final String API_ENDPOINT = blockchainPlugin.getApiEndpoint();
 
     // Found via https://v1.cosmos.network/rpc/v0.41.4
-    private static final String BALANCES_ENDPOINT = API_ENDPOINT + "/balances/%address%/by_denom?denom=%denomination%";
-    private static final String SUPPLY_ENDPOINT = API_ENDPOINT + "/supply/by_denom?denom=%denomination%";
+    private static final String BALANCES_ENDPOINT = API_ENDPOINT + "cosmos/bank/v1beta1/balances/%address%/by_denom?denom=%denomination%";
+    private static final String SUPPLY_ENDPOINT = API_ENDPOINT + "cosmos/bank/v1beta1/supply/by_denom?denom=%denomination%";
+    private static final String ACCOUNT_ENDPOINT = API_ENDPOINT + "cosmos/auth/v1beta1/accounts/%address%";
 
     // -= BALANCES =-
     public static long getBalance(String craft_address, String denomination) {
@@ -72,6 +74,16 @@ public class BlockchainRequest {
         return getTotalSupply("ucraft");
     }
 
+    public static String getAccountSequence(String craft_address) {
+        // curl -X GET "https://api.cosmos.network/cosmos/auth/v1beta1/accounts/cosmos10r39fueph9fq7a6lgswu4zdsg8t3gxlqvvvyvn" -H "accept: application/json"
+
+        String req_url = ACCOUNT_ENDPOINT.replace("%address%", craft_address);
+        System.out.println(req_url);
+        return EndpointQuery.req(req_url, RequestTypes.ACCOUNT, "Account Sequence Request").toString();
+    }
+
+    public static int acc_seq_override = 0;
+
     // -= GIVING TOKENS =-
     public static String depositToAddress(String craft_address, long amount) {        
         if(craft_address == null) {
@@ -79,21 +91,54 @@ public class BlockchainRequest {
         }
 
         // TODO: new, run a command through shell. THIS IS CURRENTLY ONLY FOR TESTING, DO NOT USE THIS IN PRODUCTION
-        // Ensure that wallet has funds
+        // This code & commands are also so yuck. Improve readability
+        // Ensure that wallet has funds? Make a script / discord notifications to send out notif when wallet funds get low
         
         /*
-        craftd keys add test --recover --keyring-backend test
+        TO NOT USE THESE MONIKERS, THEY ARE ONLY FOR PRIVATE TESTING
+        # Server Wallet to give players funds
+        craftd keys add daowallet --recover --keyring-backend test 
         multiply security charge attack minor logic staff belt want mixture sick rebuild sadness canvas twelve mango embark emotion bulb popular remind rebel circle blouse
-        # craft1s4yczg3zgr4qdxussx3wpgezangh2388xgkkz9
+        # craft1s4yczg3zgr4qdxussx3wpgezangh2388xgkkz9 -- 
+
+        # Testing that the send actually worked
+        craftd keys add playerwallet --recover --keyring-backend test
+        treat draft tip empty disorder gain symptom harbor pride motion twin right pony car bubble fantasy tube supply amused nut strong service useless expose
+        # craft12gr56gv009h8hq30k5xfv7agl2d8xa8u2ag7e4
+
+        
+        craftd tx bank send daowallet craft12gr56gv009h8hq30k5xfv7agl2d8xa8u2ag7e4 4ucraft --keyring-backend test --yes --chain-id craft-v4 --node http://65.108.125.182:26657 --sequence $(craftd q account craft1s4yczg3zgr4qdxussx3wpgezangh2388xgkkz9 --node http://65.108.125.182:26657 --output json | jq '.sequence' | sed 's/\"//g')
+        craftd q bank balances craft12gr56gv009h8hq30k5xfv7agl2d8xa8u2ag7e4 --node http://65.108.125.182:26657
         */
 
-        // String[] args = new String[] {"craftd", "tx", "bank", "send", "test", craft_address, amount+"ucraft", "--keyring-backend", "test", "--yes", "--node", "http://65.108.125.182:26657", "--chain-id", "craft-v4"};
-        String[] args = ("craftd tx bank send test " + craft_address + " " + amount + "ucraft --keyring-backend test --yes --chain-id craft-v4 --node http://65.108.125.182:26657").split(" ");
+        // when overridden, its because the sequence is off due to pending txs
+        int acc_seq;
+        if(acc_seq_override == 0) {
+            acc_seq = Integer.valueOf(getAccountSequence("craft1s4yczg3zgr4qdxussx3wpgezangh2388xgkkz9")); // DAO wallet. make sure to set in config
+        } else {
+            // override blockchain query to the correct value (if a code 32 happens)
+            acc_seq = acc_seq_override;
+            acc_seq_override = 0;
+        }
+        System.out.println("Account Sequence for DAO Wallet craft1s4yczg3zgr4qdxussx3wpgezangh2388xgkkz9: " + acc_seq);
+         
+       
+
+        // pay the user & set sequence as 1 higher than the wallets current sequence.
+        String[] args = ("craftd tx bank send daowallet " + craft_address + " " + amount + "ucraft --keyring-backend test --yes --chain-id craft-v4 --node http://65.108.125.182:26657 --sequence " + (acc_seq)).split(" ");
         try {
             // get the Tx hash here to prove it worked?
             Process process = new ProcessBuilder(args).start();
             String result = new String(process.getInputStream().readAllBytes());
             System.out.println(result);
+
+            // if the account seq is off (32) then set it to the raw logs correct value of what it expected
+            if(result.startsWith("code: 32")) {
+                // raw_log: 'account sequence mismatch, expected 13, got 12: incorrect account sequence'
+                acc_seq_override = Integer.valueOf(StringUtils.substringBetween(result, ", expected ", ", got "));
+                depositToAddress(craft_address, amount);
+            }
+            // code: 19 - already in mem pool
 
             String r2 = new String(process.getErrorStream().readAllBytes());
             System.out.println(r2);
