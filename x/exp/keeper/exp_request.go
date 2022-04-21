18b3b7310b2869fa0fcfbb276cfd2e2f2a50d613
vuong <nguyenvuong1122000@gmail.com>
2022-04-22 02:53:25 +0700

    new kv_store logic

--- x/exp/keeper/exp_request.go
+++ x/exp/keeper/exp_request.go
@@ -27,28 +27,20 @@ func (k ExpKeeper) addAddressToBurnRequestList(ctx sdk.Context, memberAccount st
 }
 
 func (k ExpKeeper) addAddressToMintRequestList(ctx sdk.Context, memberAccount sdk.AccAddress, tokenLeft sdk.Dec) error {
-	mintRequestList, err := k.GetMintRequestList(ctx)
-	if err != nil {
-		return err
-	}
 	expPrice, err := k.calculateDaoTokenPrice(ctx)
 	if err != nil {
 		return err
 	}
 
 	stableTokenLeft := tokenLeft.Mul(expPrice)
+	mintRequest := types.MintRequest{
+		Account:        memberAccount.String(),
+		DaoTokenLeft:   tokenLeft,
+		DaoTokenMinted: stableTokenLeft,
+		Status:         types.StatusOnGoingRequest,
+	}
 
-	mintRequestList.MintRequestList = append(
-		mintRequestList.MintRequestList,
-		&types.MintRequest{
-			Account:        memberAccount.String(),
-			DaoTokenLeft:   tokenLeft,
-			DaoTokenMinted: stableTokenLeft,
-			Status:         types.StatusOnGoingRequest,
-		},
-	)
-
-	k.SetMintRequestList(ctx, types.MintRequestList{MintRequestList: mintRequestList.MintRequestList})
+	k.SetMintRequest(ctx, mintRequest)
 
 	return nil
 }
@@ -68,6 +60,209 @@ func (k ExpKeeper) calculateDaoTokenValue(ctx sdk.Context, amount sdk.Int) sdk.D
 	daoTokenPrice, _ := k.calculateDaoTokenPrice(ctx)
 
 	return daoTokenPrice.MulInt(amount)
+}
+
+func (k ExpKeeper) SetBurnRequest(ctx sdk.Context, burnRequest types.BurnRequest) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&burnRequest)
+
+	accAddress, err := sdk.AccAddressFromBech32(burnRequest.Account)
+	if err != nil {
+		panic(err)
+	}
+	store.Set(types.GetBurnRequestAddressBytes(int(burnRequest.Status), accAddress), bz)
+}
+
+func (k ExpKeeper) GetBurnRequestByKey(ctx sdk.Context, key []byte) (types.BurnRequest, error) {
+	var burnRequest types.BurnRequest
+
+	store := ctx.KVStore(k.storeKey)
+	if !store.Has(key) {
+		return types.BurnRequest{}, sdkerrors.Wrapf(types.ErrInvalidKey, "burnRequest")
+	}
+
+	bz := store.Get(key)
+	err := k.cdc.Unmarshal(bz, &burnRequest)
+	if err != nil {
+		return types.BurnRequest{}, err
+	}
+
+	return burnRequest, nil
+}
+
+// GetAllBurnRequest returns all the burn request from store .
+func (keeper ExpKeeper) GetAllBurnRequests(ctx sdk.Context) (burnRequests types.BurnRequests) {
+	keeper.IterateBurnRequests(ctx, func(burnRequest types.BurnRequest) bool {
+		burnRequests = append(burnRequests, burnRequest)
+		return false
+	})
+	return
+}
+
+// IterateBurnRequest iterates over the all the BurnRequest and performs a callback function .
+func (k ExpKeeper) IterateBurnRequests(ctx sdk.Context, cb func(burnRequest types.BurnRequest) (stop bool)) {
+	store := ctx.KVStore(k.storeKey)
+
+	iterator := sdk.KVStorePrefixIterator(store, types.KeyBurnRequestList)
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		var burnRequest types.BurnRequest
+		err := k.cdc.Unmarshal(iterator.Value(), &burnRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		if cb(burnRequest) {
+			break
+		}
+	}
+}
+
+// not good logic need modify
+func (k ExpKeeper) GetBurnRequest(ctx sdk.Context, accAddress sdk.AccAddress) (types.BurnRequest, error) {
+	store := ctx.KVStore(k.storeKey)
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusCompleteRequest), accAddress)) {
+		return k.GetBurnRequestByKey(ctx, types.GetBurnRequestAddressBytes(int(types.StatusCompleteRequest), accAddress))
+	}
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusNoFundRequest), accAddress)) {
+		return k.GetBurnRequestByKey(ctx, types.GetBurnRequestAddressBytes(int(types.StatusNoFundRequest), accAddress))
+	}
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusOnGoingRequest), accAddress)) {
+		return k.GetBurnRequestByKey(ctx, types.GetBurnRequestAddressBytes(int(types.StatusOnGoingRequest), accAddress))
+	}
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusExpiredRequest), accAddress)) {
+		return k.GetBurnRequestByKey(ctx, types.GetBurnRequestAddressBytes(int(types.StatusExpiredRequest), accAddress))
+	}
+
+	return types.BurnRequest{}, sdkerrors.Wrapf(types.ErrInvalidKey, "burnRequest")
+}
+
+// IterateBurnRequest iterates over the all the BurnRequest and performs a callback function .
+func (k ExpKeeper) IterateStatusBurnRequests(ctx sdk.Context, status int, cb func(burnRequest types.BurnRequest) (stop bool)) {
+	store := ctx.KVStore(k.storeKey)
+
+	iterator := sdk.KVStorePrefixIterator(store, types.GetBurnRequestsStatusBytes(status))
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		var burnRequest types.BurnRequest
+		err := k.cdc.Unmarshal(iterator.Value(), &burnRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		if cb(burnRequest) {
+			break
+		}
+	}
+}
+
+func (k ExpKeeper) SetMintRequest(ctx sdk.Context, mintRequest types.MintRequest) {
+	store := ctx.KVStore(k.storeKey)
+	bz := k.cdc.MustMarshal(&mintRequest)
+
+	accAddress, err := sdk.AccAddressFromBech32(mintRequest.Account)
+	if err != nil {
+		panic(err)
+	}
+	store.Set(types.GetBurnRequestAddressBytes(int(mintRequest.Status), accAddress), bz)
+}
+
+func (k ExpKeeper) GetMintRequestsByStatus(ctx sdk.Context, status int) (mintRequests types.MintRequests) {
+	k.IterateStatusMintRequests(ctx, status, func(mintRequest types.MintRequest) bool {
+		mintRequests = append(mintRequests, mintRequest)
+		return false
+	})
+	return
+}
+
+// GetAllMintRequest returns all the MintRequest from store .
+func (keeper ExpKeeper) GetAllMintRequest(ctx sdk.Context) (mintRequests types.MintRequests) {
+	keeper.IterateMintRequest(ctx, func(mintRequest types.MintRequest) bool {
+		mintRequests = append(mintRequests, mintRequest)
+		return false
+	})
+	return
+}
+
+func (k ExpKeeper) GetMintRequestByKey(ctx sdk.Context, key []byte) (types.MintRequest, error) {
+	var mintRequest types.MintRequest
+
+	store := ctx.KVStore(k.storeKey)
+	if !store.Has(key) {
+		return types.MintRequest{}, sdkerrors.Wrapf(types.ErrInvalidKey, "mintRequest")
+	}
+
+	bz := store.Get(key)
+	err := k.cdc.Unmarshal(bz, &mintRequest)
+	if err != nil {
+		return types.MintRequest{}, err
+	}
+
+	return mintRequest, nil
+}
+
+// IterateMintRequest iterates over the all the MintRequest and performs a callback function .
+func (k ExpKeeper) IterateMintRequest(ctx sdk.Context, cb func(mintRequest types.MintRequest) (stop bool)) {
+	store := ctx.KVStore(k.storeKey)
+
+	iterator := sdk.KVStorePrefixIterator(store, types.KeyMintRequestList)
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		var mintRequest types.MintRequest
+		err := k.cdc.Unmarshal(iterator.Value(), &mintRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		if cb(mintRequest) {
+			break
+		}
+	}
+}
+
+// IterateStatusMintRequests iterates over the all the BurnRequest and performs a callback function .
+func (k ExpKeeper) IterateStatusMintRequests(ctx sdk.Context, status int, cb func(mintRequest types.MintRequest) (stop bool)) {
+	store := ctx.KVStore(k.storeKey)
+
+	iterator := sdk.KVStorePrefixIterator(store, types.GetBurnRequestsStatusBytes(status))
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		var mintRequest types.MintRequest
+		err := k.cdc.Unmarshal(iterator.Value(), &mintRequest)
+		if err != nil {
+			panic(err)
+		}
+
+		if cb(mintRequest) {
+			break
+		}
+	}
+}
+
+// not good logic need modify
+func (k ExpKeeper) GetMintRequest(ctx sdk.Context, accAddress sdk.AccAddress) (types.MintRequest, error) {
+	store := ctx.KVStore(k.storeKey)
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusCompleteRequest), accAddress)) {
+		return k.GetMintRequestByKey(ctx, types.GetMintRequestAddressBytes(int(types.StatusCompleteRequest), accAddress))
+	}
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusNoFundRequest), accAddress)) {
+		return k.GetMintRequestByKey(ctx, types.GetMintRequestAddressBytes(int(types.StatusNoFundRequest), accAddress))
+	}
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusOnGoingRequest), accAddress)) {
+		return k.GetMintRequestByKey(ctx, types.GetMintRequestAddressBytes(int(types.StatusOnGoingRequest), accAddress))
+	}
+
+	if store.Has(types.GetBurnRequestAddressBytes(int(types.StatusExpiredRequest), accAddress)) {
+		return k.GetMintRequestByKey(ctx, types.GetMintRequestAddressBytes(int(types.StatusExpiredRequest), accAddress))
+	}
+
+	return types.MintRequest{}, sdkerrors.Wrapf(types.ErrInvalidKey, "mintRequest")
 }
 
 func (k ExpKeeper) GetMintRequestList(ctx sdk.Context) (types.MintRequestList, error) {
---
+++ x/exp/types/request.go
@@ -0,0 +1,7 @@
+package types
+
+// MintRequests is an array of MintRequest
+type MintRequests []MintRequest
+
+// BurnRequests is an array of BurnRequest
+type BurnRequests []BurnRequest
