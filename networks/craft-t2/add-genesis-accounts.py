0c3c99e27c564a5b3260e93183eac60b8d8b5491
Reece <reecepbcups@gmail.com>
2022-05-12 10:17:57 -0500

    testnet-2 files

---
+++ networks/craft-t2/add-genesis-accounts.py
@@ -0,0 +1,81 @@
+from operator import ge
+import os
+import json
+from pathlib import Path
+
+'''
+Resets the genesis.json file to the default values & clears all balances / accounts
+Then will generate all the commands to add balances for all gentxs
+'''
+
+# cd networks/craft-t2
+LAUNCH_TIME = "2022-05-12T15:15:00Z"
+CHAIN_ID = "craft-t2"
+EXP_SEND = [{"denom": "uexp","enabled": True}]
+
+GENESIS_FILE=f"{Path.home()}/.craftd/config/genesis.json" # home dir
+FOLDER = "gentx" # accounts we will ONLY give balances too from the past submission
+
+def main():
+    outputDetails()
+    resetGenesisFile()
+    createGenesisAccountsCommands()
+    pass
+
+def resetGenesisFile():
+    # load genesis.json & remove all values for accounts & supply
+    with open(GENESIS_FILE) as f:
+        genesis = json.load(f)
+        genesis["genesis_time"] = LAUNCH_TIME
+        genesis["chain_id"] = str(CHAIN_ID)
+
+        genesis["app_state"]['auth']["accounts"] = []
+        genesis["app_state"]['bank']["balances"] = []
+        genesis["app_state"]['bank']["supply"] = []
+        genesis["app_state"]['bank']["params"]["send_enabled"] = EXP_SEND
+
+        genesis["app_state"]['genutil']["gen_txs"] = []
+
+    # save genesis.json
+    with open(GENESIS_FILE, 'w') as f:
+        json.dump(genesis, f, indent=4)
+    print(f"# RESET: {GENESIS_FILE}\n")
+
+
+def outputDetails() -> str:
+    # get the seconds until LAUNCH_TIME
+    launch_time = int(os.popen("date -d '" + LAUNCH_TIME + "' +%s").read())
+    now = int(os.popen("date +%s").read())
+    seconds_until_launch = launch_time - now
+
+    # convert seconds_until_launch to hours, minutes, and seconds
+    hours = seconds_until_launch // 3600
+    minutes = (seconds_until_launch % 3600) // 60
+
+    print(f"# {LAUNCH_TIME} ({hours}h {minutes}m) from now\n# {CHAIN_ID}\n# {EXP_SEND}\n# GenesisFile: {GENESIS_FILE}")
+
+def createGenesisAccountsCommands():
+
+    # check if FOLDER exists
+    if not os.path.exists(FOLDER):
+        print(f"# {FOLDER} not found. Did you run the script from the correct directory?")
+        print(f"# cd networks/{CHAIN_ID}")
+
+    gentx_files = os.listdir(FOLDER)
+    for file in gentx_files:
+        f = open(FOLDER + "/" + file, 'r')
+        data = json.load(f)
+
+        validatorData = data['body']['messages'][0]
+        moniker = validatorData['description']['moniker']
+        delegator = validatorData['delegator_address']
+        amt = validatorData['value']['amount']
+
+        if delegator == "craft13vhr3gkme8hqvfyxd4zkmf5gaus840j5hwuqkh":
+            print(f"craftd add-genesis-account {delegator} 100000000000000ucraft,30000000000uexp #pbcups")
+        else:
+            print(f"craftd add-genesis-account {delegator} 10000000000ucraft,{amt}uexp #{moniker}")
+
+
+if __name__ == "__main__":
+    main()
