e3dae5f5d8cbe2b053a39505c0c9cdfcd3095624
vuong <nguyenvuong1122000@gmail.com>
2022-05-29 22:54:31 +0700

    base logic in keeper

---
+++ x/nftc/keeper/keeper.go
@@ -0,0 +1,31 @@
+package keeper
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/x/nft"
+	"github.com/notional-labs/craft/x/nftc/types"
+)
+
+// Keeper of the nft store
+type Keeper struct {
+	cdc      codec.BinaryCodec
+	storeKey storetypes.StoreKey
+	bk       types.BankKeeper
+}
+
+// NewKeeper creates a new nft Keeper instance
+func NewKeeper(key storetypes.StoreKey,
+	cdc codec.BinaryCodec, ak types.AccountKeeper, bk nft.BankKeeper,
+) Keeper {
+	// ensure nft module account is set
+	if addr := ak.GetModuleAddress(types.ModuleName); addr == nil {
+		panic("the nft module account has not been set")
+	}
+
+	return Keeper{
+		cdc:      cdc,
+		storeKey: key,
+		bk:       bk,
+	}
+}
