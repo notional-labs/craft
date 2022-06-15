9eca3d88a6530622b1ae6d6b9aea407bbbf6afdb
Hieu Vu <72878483+hieuvubk@users.noreply.github.com>
2022-06-15 16:08:19 +0700

    ttest for mint, joinDao

--- x/exp/keeper/msgserver_test.go
+++ x/exp/keeper/msgserver_test.go
@@ -1,7 +1,9 @@
 package keeper_test
 
 import (
+	// "fmt"
 	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -13,7 +15,7 @@ import (
 
 var (
 	defaultAcctFunds  sdk.Coins = sdk.NewCoins(
-		sdk.NewCoin("uexp", sdk.NewInt(10000000000)),
+		// sdk.NewCoin("uexp", sdk.NewInt(10000000000)),
 		sdk.NewCoin("foo", sdk.NewInt(10000000)),
 		sdk.NewCoin("bar", sdk.NewInt(10000000)),
 		sdk.NewCoin("baz", sdk.NewInt(10000000)),
@@ -25,6 +27,7 @@ func (suite *KeeperTestSuite) TestJoinDaoByNonIbcAsset() {
 	tests := []struct {
 		fn func()
 	}{
+		// expected case
 		{
 			fn: func() {
 				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
@@ -34,9 +37,61 @@ func (suite *KeeperTestSuite) TestJoinDaoByNonIbcAsset() {
 					GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
 					MaxToken: 1000000,
 				}
-				resp, err := msgServer.JoinDaoByNonIbcAsset(c, &req)
+				_, err := msgServer.JoinDaoByNonIbcAsset(c, &req)
 				suite.Require().NoError(err)
-				fmt.Println(resp)
+
+				// check record
+				accountRecord := suite.App.ExpKeeper.GetAccountRecord(suite.Ctx, suite.TestAccs[0])
+				suite.Require().Equal(accountRecord.Account, suite.TestAccs[0].String())
+				suite.Require().Equal(accountRecord.JoinDaoTime, suite.Ctx.BlockTime())
+			},
+		},
+
+		// Each address can only be joined once
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByNonIbcAsset{
+					JoinAddress: suite.TestAccs[0].String(),
+					GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
+					MaxToken: 1000000,
+				}
+				_, err := msgServer.JoinDaoByNonIbcAsset(c, &req)
+				suite.Require().NoError(err)
+				suite.Ctx = suite.Ctx.WithBlockTime(suite.Ctx.BlockTime().Add(time.Second))
+				_, err = msgServer.JoinDaoByNonIbcAsset(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+
+		// Invalid join address
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByNonIbcAsset{
+					JoinAddress: "",
+					GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
+					MaxToken: 1000000,
+				}
+				_, err := msgServer.JoinDaoByNonIbcAsset(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+
+		// Invalid gov address
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByNonIbcAsset{
+					JoinAddress: suite.TestAccs[0].String(),
+					GovAddress: suite.TestAccs[1].String(),
+					MaxToken: 1000000,
+				}
+				_, err := msgServer.JoinDaoByNonIbcAsset(c, &req)
+				suite.Require().Error(err)
 			},
 		},
 	}
@@ -49,6 +104,214 @@ func (suite *KeeperTestSuite) TestJoinDaoByNonIbcAsset() {
 		}
 		
 
+		test.fn()
+	}
+}
+
+func (suite *KeeperTestSuite) TestJoinDaoByIbcAsset() {
+	tests := []struct {
+		fn func()
+	}{
+		// expected case
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByIbcAsset{
+					JoinAddress: suite.TestAccs[0].String(),
+					GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
+					Amount: sdk.NewDec(1000000),
+				}
+				_, err := msgServer.JoinDaoByIbcAsset(c, &req)
+				suite.Require().NoError(err)
+
+				// check record
+				mintRequest, err := suite.App.ExpKeeper.GetMintRequest(suite.Ctx, suite.TestAccs[0])
+				suite.Require().Equal(mintRequest.Account, suite.TestAccs[0].String())
+				suite.Require().Equal(mintRequest.DaoTokenLeft, sdk.NewDec(1000000))
+				suite.Require().Equal(mintRequest.DaoTokenMinted, sdk.NewDec(0))
+				suite.Require().Equal(mintRequest.RequestTime, suite.Ctx.BlockTime())
+				suite.Require().Equal(mintRequest.Status, types.StatusOnGoingRequest)
+			},
+		},
+
+		// Each address can only be joined once
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByIbcAsset{
+					JoinAddress: suite.TestAccs[0].String(),
+					GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
+					Amount: sdk.NewDec(1000000),
+				}
+				_, err := msgServer.JoinDaoByIbcAsset(c, &req)
+				suite.Require().NoError(err)
+				suite.Ctx = suite.Ctx.WithBlockTime(suite.Ctx.BlockTime().Add(time.Second))
+				_, err = msgServer.JoinDaoByIbcAsset(c, &req)
+				suite.Require().Error(err) // should be err here
+			},
+		},
+
+		// Invalid join address
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByIbcAsset{
+					JoinAddress: "",
+					GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
+					Amount: sdk.NewDec(1000000),
+				}
+				_, err := msgServer.JoinDaoByIbcAsset(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+
+		// Invalid gov address
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgJoinDaoByIbcAsset{
+					JoinAddress: suite.TestAccs[0].String(),
+					GovAddress: suite.TestAccs[1].String(),
+					Amount: sdk.NewDec(1000000),
+				}
+				_, err := msgServer.JoinDaoByIbcAsset(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest()
+		// Mint some assets to the accounts.
+		for _, acc := range suite.TestAccs {
+			suite.FundAcc(acc, defaultAcctFunds)
+		}
+		
+
+		test.fn()
+	}
+}
+
+func (suite *KeeperTestSuite) TestMintAndAllocateExp() {
+	tests := []struct {
+		fn func()
+	}{
+		// expected case
+		{
+			fn: func() {
+				balance := suite.App.BankKeeper.GetBalance(suite.Ctx, suite.TestAccs[0], "uexp")
+				suite.Require().Equal(balance.Amount, sdk.NewInt(0))
+
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				balances := suite.App.BankKeeper.GetAccountsBalances(suite.Ctx)
+				fmt.Println(balances)
+				req := types.MsgMintAndAllocateExp{
+					Amount: sdk.NewCoins(sdk.NewCoin("uexp", sdk.NewInt(1000000))),
+					FromAddress: daoAddress,
+					Member: suite.TestAccs[0].String(),
+				}
+				_, err := msgServer.MintAndAllocateExp(c, &req)
+				suite.Require().NoError(err)
+
+				//check balances after mint executed
+				balance = suite.App.BankKeeper.GetBalance(suite.Ctx, suite.TestAccs[0], "uexp")
+				suite.Require().Equal(balance.Amount, sdk.NewInt(1000000))
+			},
+		},
+
+		// Only mint to member in whitelist
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgMintAndAllocateExp{
+					Amount: sdk.NewCoins(sdk.NewCoin("uexp", sdk.NewInt(1000000))),
+					FromAddress: daoAddress,
+					Member: suite.TestAccs[1].String(),
+				}
+				_, err := msgServer.MintAndAllocateExp(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+
+		// Only Dao address can excute mint msg
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgMintAndAllocateExp{
+					Amount: sdk.NewCoins(sdk.NewCoin("uexp", sdk.NewInt(1000000))),
+					FromAddress: suite.TestAccs[1].String(),
+					Member: suite.TestAccs[0].String(),
+				}
+				_, err := msgServer.MintAndAllocateExp(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+
+		// Amount must be < MaxAmount
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+				req := types.MsgMintAndAllocateExp{
+					Amount: sdk.NewCoins(sdk.NewCoin("uexp", sdk.NewInt(10000000))),
+					FromAddress: daoAddress,
+					Member: suite.TestAccs[0].String(),
+				}
+				_, err := msgServer.MintAndAllocateExp(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+
+		// Only mint exp
+		{
+			fn: func() {
+				msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+				c := sdk.WrapSDKContext(suite.Ctx)
+
+				// try to mint one token but not exp
+				req := types.MsgMintAndAllocateExp{
+					Amount: sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(1000000))),
+					FromAddress: daoAddress,
+					Member: suite.TestAccs[0].String(),
+				}
+				_, err := msgServer.MintAndAllocateExp(c, &req)
+				suite.Require().Error(err)
+
+				// try to mint more than one token but contain exp
+				req = types.MsgMintAndAllocateExp{
+					Amount: sdk.NewCoins(sdk.NewCoin("uosmo", sdk.NewInt(1000000)), sdk.NewCoin("uexp", sdk.NewInt(1000000))),
+					FromAddress: daoAddress,
+					Member: suite.TestAccs[0].String(),
+				}
+				_, err = msgServer.MintAndAllocateExp(c, &req)
+				suite.Require().Error(err)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		suite.SetupTest()
+		// Mint some assets to the accounts.
+		for _, acc := range suite.TestAccs {
+			suite.FundAcc(acc, defaultAcctFunds)
+		}
+		msgServer := keeper.NewMsgServerImpl(suite.App.ExpKeeper)
+		c := sdk.WrapSDKContext(suite.Ctx)
+		req := types.MsgJoinDaoByNonIbcAsset{
+			JoinAddress: suite.TestAccs[0].String(),
+			GovAddress: suite.App.AccountKeeper.GetModuleAccount(suite.Ctx, govtypes.ModuleName).GetAddress().String(),
+			MaxToken: 1000000,
+		}
+		_, err := msgServer.JoinDaoByNonIbcAsset(c, &req)
+		suite.Require().NoError(err)
+		
 		test.fn()
 	}
 }
