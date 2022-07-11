09bd8a39e254c2ce4f041a548b6f5aaac487a6ec
Reece Williams <reecepbcups@gmail.com>
2022-07-11 09:10:10 -0500

    NFTS: Updates schema

--- NFTs/marketplace/examples/schema.rs
+++ NFTs/marketplace/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use nftext_manager::msg::{BuyNft, HandleMsg, InitMsg, QueryMsg, SellNft};
-use nftext_manager::package::{ContractInfoResponse, OfferingsResponse, QueryOfferingsResult};
+use craft_marketplace::msg::{HandleMsg, InitMsg, QueryMsg, SellNft};
+use craft_marketplace::package::{ContractInfoResponse, OfferingsResponse, QueryOfferingsResult};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -16,7 +16,6 @@ fn main() {
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(SellNft), &out_dir);
-    export_schema(&schema_for!(BuyNft), &out_dir);
     export_schema(&schema_for!(OfferingsResponse), &out_dir);
     export_schema(&schema_for!(ContractInfoResponse), &out_dir);
     export_schema(&schema_for!(QueryOfferingsResult), &out_dir);
