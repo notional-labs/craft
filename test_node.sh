60b51f227a43cdfb956532628d899868ef969a2e
Reece Williams <reecepbcups@gmail.com>
2022-07-16 16:16:20 -0500

    test_node: ability to start from current height

--- test_node.sh
+++ test_node.sh
@@ -8,14 +8,11 @@ LOGLEVEL="info"
 #TRACE="--trace"
 TRACE=""
 
-# validate dependencies are installed
-command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
-
-# remove existing daemon
-rm -rf ~/.craft*
-
 craftd config keyring-backend $KEYRING
 craftd config chain-id $CHAINID
+
+# validate dependencies are installed
+command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
 # if $KEY exists it should be deleted
 # decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry
@@ -31,38 +28,47 @@ update_test_genesis () {
 }
 
 
-# Set gas limit in genesis
-update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
-update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"]="15s"'
+from_scratch () {
+  # remove existing daemon
+  rm -rf ~/.craft* 
 
-# Change chain options to use EXP as the staking denom for craft
-update_test_genesis '.app_state["staking"]["params"]["bond_denom"]="uexp"'
-update_test_genesis '.app_state["bank"]["params"]["send_enabled"]=[{"denom": "uexp","enabled": true}]'
-update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.050000000000000000"'
+  # Set gas limit in genesis
+  update_test_genesis '.consensus_params["block"]["max_gas"]="100000000"'
+  update_test_genesis '.app_state["gov"]["voting_params"]["voting_period"]="15s"'
 
-# update from token -> ucraft
-update_test_genesis '.app_state["mint"]["params"]["mint_denom"]="ucraft"'
-update_test_genesis '.app_state["exp"]["params"]["ibc_asset_denom"]="ucraft"'
-update_test_genesis '.app_state["gov"]["deposit_params"]["min_deposit"]=[{"denom": "ucraft","amount": "10000000"}],'
-update_test_genesis '.app_state["crisis"]["constant_fee"]={"denom": "ucraft","amount": "1000"}'
+  # Change chain options to use EXP as the staking denom for craft
+  update_test_genesis '.app_state["staking"]["params"]["bond_denom"]="uexp"'
+  update_test_genesis '.app_state["bank"]["params"]["send_enabled"]=[{"denom": "uexp","enabled": true}]'
+  update_test_genesis '.app_state["staking"]["params"]["min_commission_rate"]="0.050000000000000000"'
+
+  # update from token -> ucraft
+  update_test_genesis '.app_state["mint"]["params"]["mint_denom"]="ucraft"'
+  update_test_genesis '.app_state["exp"]["params"]["ibc_asset_denom"]="ucraft"'
+  update_test_genesis '.app_state["gov"]["deposit_params"]["min_deposit"]=[{"denom": "ucraft","amount": "10000000"}],'
+  update_test_genesis '.app_state["crisis"]["constant_fee"]={"denom": "ucraft","amount": "1000"}'
 
 
-# Allocate genesis accounts (cosmos formatted addresses)
-craftd add-genesis-account $KEY 1000000000000000000000000uexp,10000000000ucraft --keyring-backend $KEYRING
-# Adds token to reece
-craftd add-genesis-account craft10r39fueph9fq7a6lgswu4zdsg8t3gxlqd6lnf0 8000000uexp,10000000000ucraft --keyring-backend $KEYRING
-# add tokens to webappt team
-craftd add-genesis-account craft1sv434uclts5u7ufrzqsmqvlxhkw04q84yuh0hj 1000000uexp,10000000000ucraft --keyring-backend $KEYRING
-craftd add-genesis-account craft14mt78hz73d9tdwpdvkd59ne9509kxw8y53sjt9 1000000uexp,10000000000ucraft --keyring-backend $KEYRING
+  # Allocate genesis accounts (cosmos formatted addresses)
+  // 1 million EXP
+  craftd add-genesis-account $KEY 1000000000000uexp,10000000000ucraft --keyring-backend $KEYRING
+  # Adds token to reece
+  craftd add-genesis-account craft10r39fueph9fq7a6lgswu4zdsg8t3gxlqd6lnf0 8000000uexp,10000000000ucraft --keyring-backend $KEYRING
+  # add tokens to webappt team
+  craftd add-genesis-account craft1sv434uclts5u7ufrzqsmqvlxhkw04q84yuh0hj 1000000uexp,10000000000ucraft --keyring-backend $KEYRING
+  craftd add-genesis-account craft14mt78hz73d9tdwpdvkd59ne9509kxw8y53sjt9 1000000uexp,10000000000ucraft --keyring-backend $KEYRING
 
-# Sign genesis transaction
-craftd gentx $KEY 1000000000000000000000uexp --keyring-backend $KEYRING --chain-id $CHAINID
+  # Sign genesis transaction
+  craftd gentx $KEY 1000000000000uexp --keyring-backend $KEYRING --chain-id $CHAINID
 
-# Collect genesis tx
-craftd collect-gentxs
+  # Collect genesis tx
+  craftd collect-gentxs
 
-# Run this to ensure everything worked and that the genesis file is setup correctly
-craftd validate-genesis
+  # Run this to ensure everything worked and that the genesis file is setup correctly
+  craftd validate-genesis
+}
+
+# from_scratch
+
 
 if [[ $1 == "pending" ]]; then
   echo "pending mode is on, please wait for the first block committed."
@@ -70,7 +76,8 @@ fi
 
 # Opens the RPC endpoint to outside connections
 sed -i '/laddr = "tcp:\/\/127.0.0.1:26657"/c\laddr = "tcp:\/\/0.0.0.0:26657"' ~/.craftd/config/config.toml
-sed -i 's/cors-allowed-origins = \[\]/cors-allowed-origins = \["\*"\]/g' ~/.craftd/config/config.toml
+sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["\*"\]/g' ~/.craftd/config/config.toml
+# cors_allowed_origins = []
 
 # # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
 craftd start --pruning=nothing  --minimum-gas-prices=0ucraft #--mode validator       
