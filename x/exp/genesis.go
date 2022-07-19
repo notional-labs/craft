1604ee706f7259e1ab01282170f5b912d0cfb483
vuong <nguyenvuong1122000@gmail.com>
2022-07-19 09:41:56 +0700

    bind port when genesis

--- x/exp/genesis.go
+++ x/exp/genesis.go
@@ -17,6 +17,16 @@ func InitGenesis(ctx sdk.Context, keeper keeper.ExpKeeper, ak types.AccountKeepe
 
 	// for IBC
 	keeper.SetPort(ctx, data.PortId)
+	// Only try to bind to port if it is not already bound, since we may already own
+	// port capability from capability InitGenesis
+	if !keeper.IsBound(ctx, data.PortId) {
+		// module binds to the port on InitChain
+		// and claims the returned capability
+		err := keeper.BindPort(ctx, data.PortId)
+		if err != nil {
+			panic("could not claim port capability: " + err.Error())
+		}
+	}
 	keeper.SetDaoAssetInfo(ctx, *data.DaoAsset)
 	ak.GetModuleAccount(ctx, types.ModuleName)
 }
