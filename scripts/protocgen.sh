be7c75b4443a394151ef0248bc124ebe5b248839
GNaD <cpt.meoz@gmail.com>
2022-08-11 23:40:04 +0700

    remove unused command

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -24,12 +24,6 @@ cd ..
 # move proto files to the right places
 #
 # Note: Proto files are suffixed with the current binary version.
-#cp -r github.com/osmosis-labs/osmosis/v7/* ./
-#rm -rf github.com
 
 go mod tidy -compat=1.18
 
-# TODO: Uncomment once ORM/Pulsar support is needed.
-#
-# Ref: https://github.com/osmosis-labs/osmosis/pull/1589
-# ./scripts/protocgen2.sh
